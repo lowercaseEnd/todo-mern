a2bda8ebc7c28e74fa228e85478a2f60c5ac9bf5
John Doe <randomEme@protonmail.com>
2020-04-16 11:16:33 +0400

    Добавил react-bootstrap и сделал выпадающее меню

--- src/App.js
+++ src/App.js
@@ -2,40 +2,35 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./logo.svg";
+import { Container, Navbar, Nav, Row, Col } from "react-bootstrap";
 
 import CreateTodo from "./create-todo";
 import EditTodo from "./edit-todo";
 import TodosList from "./todos-list";
-{/* <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link> */}
+{/* <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link> */ }
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">
+        <Container>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#">
               <img src={logo} width="30" height="30" alt="React logo" />
-            </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse"  data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            
-            <div className="collapse navbar-collapse" id="navbarContent">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <Link to="/" className="nav-link">Todos</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarContent" />
+
+            <Navbar.Collapse id="navbarContent">
+              <Nav className="mr-auto">
+                <Link to="/" className="nav-link">Todos</Link>
+                <Link to="/create" className="nav-link">Create Todo</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
           <br />
           <Route path="/" exact component={TodosList} />
           <Route path="/edit/:id" component={EditTodo} />
           <Route path="/create" component={CreateTodo} />
-        </div>
+        </Container>
       </Router>
 
     );
